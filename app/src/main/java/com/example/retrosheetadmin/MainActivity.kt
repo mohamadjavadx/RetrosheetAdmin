ed28cf7e7d2575b8d68d54bdb55d97cb8d2e94c2
Mohamadjavad Pourmoradian <mohamadjavad.pourmoradian@gmail.com>
2021-09-25 19:35:33 +0330

    init

--- app/src/main/java/com/example/retrosheetadmin/MainActivity.kt
+++ app/src/main/java/com/example/retrosheetadmin/MainActivity.kt
@@ -4,31 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.example.retrosheetadmin.datasource.ImageRepository
-import com.example.retrosheetadmin.ui.imagelist.ImageListComposable
-import com.example.retrosheetadmin.ui.imagelist.ImageListEvents
+import com.example.retrosheetadmin.model.Image
+import com.example.retrosheetadmin.ui.imagelist.ImageCardComposable
 import com.example.retrosheetadmin.ui.imagelist.ImageListState
 import com.example.retrosheetadmin.ui.imagelist.ImageSheetViewModel
 import com.example.retrosheetadmin.ui.theme.RetrosheetAdminTheme
 import com.example.retrosheetadmin.util.log
-import com.google.accompanist.swiperefresh.SwipeRefresh
-import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
+import java.util.*
 import javax.inject.Inject
 
+@ExperimentalAnimationApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
@@ -41,60 +43,56 @@ class MainActivity : ComponentActivity() {
         setContent {
             RetrosheetAdminTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.fillMaxSize()
+                ) {
 
                     val viewModel: ImageSheetViewModel by viewModels()
-                    val state : ImageListState = viewModel.state.value
+                    val state: ImageListState = viewModel.state.value
 
-                    ImageListComposable(
-                        state,
-                        onTriggerEvent = {
-                            viewModel.onTriggerEvent(it)
-                        }
-                    )
-
-//                    val state = viewModel.state.value
-//                    if(state is ImageListState.Success)
-//                    ImageListComposable(images = state.imageList.log()) {
-//                        viewModel.onTriggerEvent(ImageListEvents.DeleteImage(it))
-//                    }
-
-
-
-
-
-//                    Column {
-//
-//                        when(val state = viewModel.state.value){
-//
-//                            is ImageListState.Loading -> {
-//                                CircularProgressIndicator()
-//
-//                            }
-//
-//                            is ImageListState.Success -> {
-//                                ImageListComposable(images = state.imageList.log()) {
-//                                    viewModel.onTriggerEvent(ImageListEvents.DeleteImage(it))
-//                                }
-//                            }
-//
-//                            is ImageListState.Error -> {
-//
-//                            }
-//
-//                            !is ImageListState.Loading -> {
-//                                Button(onClick = {
-//                                    viewModel.onTriggerEvent(ImageListEvents.LoadImages)
-//                                }) {
-//                                    Text(text = "refresh")
-//                                }
-//                            }
-//
-//                            is ImageListState.Non -> Unit
-//
+//                    ImageListComposable(
+//                        state,
+//                        onTriggerEvent = {
+//                            viewModel.onTriggerEvent(it)
 //                        }
-//
-//                    }
+//                    )
+
+                    val list: ArrayList<Int> = arrayListOf(
+                        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                    )
+                    var expandedItem by remember { mutableStateOf(-1) }
+
+                    if (state is ImageListState.Success) {
+                        val imageList: List<Image> = arrayListOf<Image>().apply {
+                            addAll(state.imageList)
+                            addAll(state.imageList)
+                            addAll(state.imageList)
+                            addAll(state.imageList)
+                        }
+                        LazyColumn {
+                            itemsIndexed(imageList) { index: Int, item: Image ->
+                                Spacer(modifier = Modifier.height(8.dp))
+                                ImageCardComposable(
+                                    image = item,
+                                    index = index,
+                                    isExpanded = expandedItem == index,
+                                    onClickMore = { isExpanded ->
+                                        expandedItem = if (isExpanded) index else -1
+                                    },
+                                    onClickDelete = {},
+                                    onClickUpdate = {}
+                                )
+                            }
+                        }
+                    }
+
+
+//                    ImageCardComposable(image = null, onClickDelete = {},onClickUpdate = {})
+
                 }
             }
         }
--- app/src/main/java/com/example/retrosheetadmin/ui/imagelist/ImageCardComposable.kt
+++ app/src/main/java/com/example/retrosheetadmin/ui/imagelist/ImageCardComposable.kt
@@ -1,18 +1,24 @@
 package com.example.retrosheetadmin.ui.imagelist
 
+import android.net.Uri
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.Top
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.retrosheetadmin.model.Image
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.ui.platform.LocalContext
 
 
 @Composable
@@ -37,7 +43,6 @@ fun ImageCardComposable(
 
                     Text(text = "link:")
 
-                    Text(text = "createdAt:")
                 }
 
                 Column(modifier = Modifier.padding(8.dp)) {
@@ -51,11 +56,9 @@ fun ImageCardComposable(
                         }
                     )
 
-                    Text(text = image.createdAt)
                 }
 
             }
-
 
             Button(onClick = { onClickDeleteButton.invoke() }) {
                 Text(text = "delete")
@@ -63,6 +66,118 @@ fun ImageCardComposable(
 
         }
 
+    }
+}
+
+
+@ExperimentalAnimationApi
+@Composable
+fun ImageCardComposable(
+    image: Image,
+    index: Int,
+    isExpanded: Boolean,
+    onClickMore: (isExpanded: Boolean) -> Unit,
+    onClickDelete: () -> Unit,
+    onClickUpdate: () -> Unit,
+) {
+
+
+    Card(
+        modifier = Modifier
+            .padding(top = 0.dp, bottom = 0.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        shape = MaterialTheme.shapes.medium.copy(all = CornerSize(0.dp)),
+        elevation = 4.dp
+
+    ) {
+
+        val current = LocalContext.current
+
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+        ) {
+
+            Row {
+
+                Column(
+                    modifier = Modifier
+                        .wrapContentHeight()
+                        .weight(1f, true)
+                ) {
+
+                    Text(
+                        text = "title: ${image.title}",
+                        style = MaterialTheme.typography.subtitle1
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        text = "link: ${image.link}",
+                        style = MaterialTheme.typography.subtitle1,
+                        color = MaterialTheme.colors.secondary,
+                        modifier = Modifier.clickable {
+                            val builder = CustomTabsIntent.Builder()
+                            val customTabsIntent = builder.build()
+                            customTabsIntent.launchUrl(current, Uri.parse(image.link))
+                        }
+                    )
+
+                }
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                IconButton(
+                    modifier = Modifier.align(Top),
+                    onClick = { onClickMore(!isExpanded) }
+                ) {
+                    Crossfade(targetState = isExpanded) {
+                        Icon(
+                            imageVector = if (it) Icons.Rounded.Close else Icons.Rounded.MoreVert,
+                            contentDescription = "",
+                            tint = MaterialTheme.colors.secondary
+                        )
+                    }
+                }
+
+            }
+
+            AnimatedVisibility(isExpanded) {
+
+                Column {
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+
+                        OutlinedButton(
+                            colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colors.onSurface),
+                            onClick = { onClickDelete() }
+                        ) {
+                            Text(text = "Delete")
+                        }
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        OutlinedButton(
+                            colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colors.onSurface),
+                            onClick = { onClickUpdate() }
+                        ) {
+                            Text(text = "Update")
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
 
     }
 }
